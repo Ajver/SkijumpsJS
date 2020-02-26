6f9d4eaed32fa6cebc753df4cb1da78ecb19cdf4
Ajver <zawlockidominik@gmail.com>
2020-02-26 20:57:33 +0100

    Better background on TitanBase

--- experimental/skijump-js/sj-objects/sj-Camera.js
+++ experimental/skijump-js/sj-objects/sj-Camera.js
@@ -3,10 +3,10 @@ SJ.Camera =
 class {
   constructor(scaleMod) {
     // this._offset = createVector(
-    //   -500,
-    //   -200
-    // )
-    this._offset = createVector(-200, 0)
+    //   -5600 * scaleMod,
+    //   -2800 * scaleMod
+    // );
+    this._offset = createVector(-200, 0);
     this._offset.x += SJ.SCREEN_WIDTH*0.5;
     this._offset.y += SJ.SCREEN_HEIGHT*0.5;
     this._scale = scaleMod;
--- experimental/skijump-js/sj-spacejump.js
+++ experimental/skijump-js/sj-spacejump.js
@@ -5,7 +5,7 @@ const SJ = {};
 SJ.UI = {};
 
 // Game Version
-SJ.VERSION = "0.22.10";
+SJ.VERSION = "0.22.11";
 
 // Screen resolution
 SJ.SCREEN_WIDTH = 1200;
