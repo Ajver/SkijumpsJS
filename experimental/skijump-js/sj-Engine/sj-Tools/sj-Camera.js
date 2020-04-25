2e1f5a6f13768581196d426eecf75781d6404921
Szymon1111 <40290583+Szymon1111@users.noreply.github.com>
2020-04-25 17:24:21 +0200

    Finish cyber city add tracking drone

--- experimental/skijump-js/sj-Engine/sj-Tools/sj-Camera.js
+++ experimental/skijump-js/sj-Engine/sj-Tools/sj-Camera.js
@@ -3,8 +3,8 @@ SJ.Camera =
 class {
   constructor(scaleMod) {
     // this._offset = createVector(
-    //   -8800 * scaleMod,
-    //   -5000 * scaleMod
+    //   -4500 * scaleMod,
+    //   -1500 * scaleMod
     // );
     this._offset = createVector(-200, 0);
     this._offset.x += SJ.SCREEN_WIDTH*0.5;
