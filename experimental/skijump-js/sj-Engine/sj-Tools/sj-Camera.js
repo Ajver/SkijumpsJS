ee8ac3e912f7abb3f1f7b264452d936b078dbb14
Szymon1111 <40290583+Szymon1111@users.noreply.github.com>
2020-06-17 23:59:49 +0200

    prace nad starstation 70% gotowe

--- experimental/skijump-js/sj-Engine/sj-Tools/sj-Camera.js
+++ experimental/skijump-js/sj-Engine/sj-Tools/sj-Camera.js
@@ -3,8 +3,8 @@ SJ.Camera =
 class {
   constructor(scaleMod) {
     this._offset = createVector(
-      -7300 * scaleMod,
-      -4100 * scaleMod
+      -9000 * scaleMod,
+      -3800 * scaleMod
     );
     this._offset = createVector(-200, 0);
     this._offset.x += SJ.SCREEN_WIDTH*0.5;
